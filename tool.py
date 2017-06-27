e4d5e1b6e1937194d92f4b4fb3ee39b265ded7ee
maker001 <806160921@qq.com>
2017-06-27 14:22:37 +0800

    add photos

--- tool.py
+++ tool.py
@@ -128,7 +128,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("https://github.com/maker997/backupBlog/source/photos/data.json","w") as fp:
+    with open("./source/photos/data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
