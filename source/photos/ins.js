8af1675e3194aa355688f5273a00dd228827462e
maker001 <806160921@qq.com>
2017-06-27 15:48:26 +0800

    add photos

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,7 +116,6 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          
           var minSrc = 'https://raw.githubusercontent.com/maker997/backupBlog/master/min_photos/' + data.link[i];
           var src = 'https://raw.githubusercontent.com/maker997/backupBlog/master/photos/' + data.link[i];
           var type = data.type[i];
